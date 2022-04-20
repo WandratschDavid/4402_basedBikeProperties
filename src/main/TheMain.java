c0328a6cb4fcf37560e807c0116790c9b3525f02
WandratschDavid <72546181+David291202@users.noreply.github.com>
2022-04-21 00:41:58 +0200

    initial commit

---
+++ src/main/TheMain.java
@@ -0,0 +1,13 @@
+package main;
+
+/**
+ * @author David Wandratsch, 4BHIT
+ * @version 1.0, class TheMain
+ */
+public class TheMain
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World!");
+	}
+}
